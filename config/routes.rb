56104e27d517d6d5d89419f572d3d389cd86d67a
Luis Castillo <castillovaliente@gmail.com>
2019-11-30 15:19:32 -0600

    setting gems for testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  resources :favorite_jobs, only: [:index, :create, :destroy]
+  
   root to: 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
